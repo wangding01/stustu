c5763e2fea3951c36c00ec8d5e480bece3400898
wangding01 <632848208@qq.com>
2019-06-21 15:23:02 +0800

    新增日志输出logback-spring。xml

--- xybank/src/main/java/com/shuttle/chat/ChatController.java
+++ xybank/src/main/java/com/shuttle/chat/ChatController.java
@@ -1,19 +1,26 @@
 package com.shuttle.chat;
 
-import java.net.UnknownHostException;
-
-import javax.servlet.http.HttpServletRequest;
-
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.util.StringUtils;
+
+import com.shuttle.common.utils.MD5Utils;
+import com.shuttle.common.utils.R;
 
 @RestController
 @RequestMapping("/web/chat")
 public class ChatController {
+	private Logger log = LoggerFactory.getLogger(ChatController.class);
 	@Value("${natapp.urlhost}")
 	private String natappUrl;
 
@@ -28,15 +35,26 @@ public class ChatController {
 	/**
 	 * 聊天界面
 	 */
-	@GetMapping("/index")
-	public ModelAndView index(String username, String password, HttpServletRequest request)
-			throws UnknownHostException {
-		if (StringUtils.isEmpty(username)) {
-			username = "匿名用户";
+	@PostMapping("/index")
+	@ResponseBody
+	R index(String username, String password) {
+		password = MD5Utils.encrypt(username, password);
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+		Subject subject = SecurityUtils.getSubject();
+		try {
+			subject.login(token);
+			ModelAndView mav = new ModelAndView("/chat/chat");
+			mav.addObject("username", username);
+			mav.addObject("webSocketUrl", "ws://" + natappUrl + "/chat");
+			return R.ok();
+		} catch (AuthenticationException e) {
+			return R.error("用户或密码错误");
 		}
-		ModelAndView mav = new ModelAndView("/chat/chat");
-		mav.addObject("username", username);
-		mav.addObject("webSocketUrl", "ws://" + natappUrl + "/chat");
-		return mav;
+	}
+
+	@GetMapping("/testlog")
+	public String testlog() {
+		log.debug("这是debug日志");
+		return "";
 	}
 }
